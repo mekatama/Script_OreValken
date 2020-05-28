cf2c38f80676bf6a39a1fa715ca5e48ccd6fe8e7
mekatama <tamatte@hotmail.com>
2020-05-28 13:00:50 +0900

    01 左右移動

---
+++ PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour{
+	public float speed = 10.0f;
+	public Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		float z =  Input.GetAxis("Horizontal") * speed;
+		//実際に力を加える
+		rb.AddForce(0 , 0 , z );
+	}
+}
