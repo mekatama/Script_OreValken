a86f0f74eb96f3005ef5f19cf3e9dd3256120c3f
mekatama <tamatte@hotmail.com>
2020-05-31 19:14:41 +0900

    07 jumpの基本挙動

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour{
-	public float speed = 10.0f;
-	public Rigidbody rb;
+	public float speed = 10.0f;		//移動speed
+	public float jumpPower = 250.0f;//jump力
+	public Rigidbody rb;			//Rigidbody入れる用
+	private bool isGround;			//接着flag
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate() {
+		//左右入力
 		float z =  Input.GetAxis("Horizontal") * speed;
+		//jump入力
+		if(isGround == true){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				isGround = false;
+				//実際に力を加える
+				rb.AddForce(Vector3.up * jumpPower);
+			}
+		}
 		//実際に力を加える
 		rb.AddForce(0 , 0 , z );
+	}
+
+	//接着判定
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "Ground"){
+			isGround = true;
+		}
 	}
 }
