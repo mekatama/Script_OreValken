4810eb0d8d62cea7a6620c142c3e6994644b2cb8
mekatama <tamatte@hotmail.com>
2020-06-04 00:15:17 +0900

    09 jump motion 再生

--- PlayerAnimation.cs
+++ PlayerAnimation.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour{
 	private Vector3 velocity;	//判定用のベクトル入れる用
 	private Animator animater;	//Animater入れる用
+	private PlayerMove moveScript;	//PlayerMoveを参照
 
 	void Start(){
 		animater = GetComponent<Animator>();	//Animater取得
+		moveScript = GetComponentInParent<PlayerMove>();//上の階層のオブジェクトのPlayerMove.cs参照
 	}
 
     void Update(){
@@ -22,5 +24,15 @@ public class PlayerAnimation : MonoBehaviour{
 			//idol motion
 			animater.SetFloat("speed",0f);
 		}
+
+		//jump motion制御
+		if(moveScript.isGround == false){
+			//jump motion
+			animater.SetBool("jump",true);
+		}else{
+			//idol motion
+			animater.SetBool("jump",false);
+		}
+
 	}
 }
--- PlayerMove.cs
+++ PlayerMove.cs
@@ -6,13 +6,15 @@ public class PlayerMove : MonoBehaviour{
 	public float speed = 10.0f;		//移動speed
 	public float jumpPower = 250.0f;//jump力
 	public Rigidbody rb;			//Rigidbody入れる用
-	private bool isGround;			//接着flag
+	public bool isGround;			//接着flag
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+		isGround = false;	//初期化
 	}
 
 	void FixedUpdate() {
+		Debug.Log(isGround);
 		//左右入力
 		float z =  Input.GetAxis("Horizontal") * speed;
 		//jump入力
