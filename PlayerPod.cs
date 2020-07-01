0cef40714bdada5748351d7d57850468332806d8
mekatama <tamatte@hotmail.com>
2020-07-01 23:46:55 +0900

    podのキー入力で角度制御

---
+++ PlayerPod.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPod : MonoBehaviour{
+	private float speed = 1.8f;	//角度変化速度調整用
+	void Start(){
+	}
+
+	void Update(){
+		gameObject.transform.Rotate(Input.GetAxis("Vertical") * speed, 0, 0);
+	}
+}
