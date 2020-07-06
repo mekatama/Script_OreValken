abaa0da3741274ae01a7cc8adf63b6e5a0adc25f
mekatama <tamatte@hotmail.com>
2020-07-06 23:58:48 +0900

    18 弾の前進

---
+++ BulletMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour{
+	public float speed = 0.0f;	//弾の移動速度
+	void Start(){
+	}
+
+	void Update(){
+		//移動
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+}
