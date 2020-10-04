02cde273efefc5c2c5c23b63056b1055693a1a26
mekatama <tamatte@hotmail.com>
2020-10-04 17:19:18 +0900

    19 bullet消す

---
+++ BulletDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour{
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	//他のオブジェクトとの当たり判定(trigger)
+	void OnTriggerEnter( Collider other) {
+		if(other.tag == "Ground"){
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+		if(other.tag == "Wall"){
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+	}
+
+}
